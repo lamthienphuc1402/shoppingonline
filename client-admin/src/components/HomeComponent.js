26b62d12663f99ac9db2acfd0614bbb81688f51a
lamthienphuc1402 <123019955+lamthienphuc1402@users.noreply.github.com>
2023-11-30 09:54:45 +0700

    HomeComponent.js

--- client-admin/src/components/HomeComponent.js
+++ client-admin/src/components/HomeComponent.js
@@ -5,7 +5,7 @@ class Home extends Component {
         return (
             <div className="align-center">
                 <h2 className="text-center">ADMIN HOME</h2>
-                <img src="http://cliparting.com/wp-content/uploads/2018/03/animated-emoticons-2018-13.gif" width="800px" height="600px" alt="" />
+                <img src="https://img.freepik.com/free-vector/settings-concept-illustration_114360-3754.jpg" alt="" />
             </div>
         );
     }
